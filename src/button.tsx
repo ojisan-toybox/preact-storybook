8d2ae78a4fcecad3980c2f0db45b6b3b1abdce03
sadness_ojisan <sadness.ojisan@gmail.com>
2021-01-16 21:24:00 +0900

    add button style

--- src/button.tsx
+++ src/button.tsx
@@ -1,5 +1,12 @@
 import { h } from "preact";
+import { setup, styled } from "goober";
+
+setup(h);
+
+const _Button = styled("button")`
+  background-color: red;
+`;
 
 export const Button = () => {
-  return <button>ok</button>;
+  return <_Button>ok</_Button>;
 };
